4f875f2cecae0bf133d9cda7f38c0edce51f747e
anuj babu <25268716+ShwethaVasudevan@users.noreply.github.com>
2017-12-17 23:28:02 +0530

    first commit

--- DiceGame/ViewController.swift
+++ DiceGame/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sumLabel: UILabel!
+    @IBOutlet weak var leftDie: UIImageView!
+    @IBOutlet weak var rightDie: UIImageView!
+    @IBOutlet weak var total: UILabel!
+    
+    var dieArray = ["Dice1","Dice2","Dice3","Dice4","Dice5","Dice6"];
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonClick(_ sender: UIButton) {
+        
+        let leftNumber = Int(arc4random_uniform(6))
+        let rightNumber = Int(arc4random_uniform(6))
+        
+        let sum:Int = leftNumber + rightNumber + 2
+        
+        leftDie.image = UIImage(named: dieArray[leftNumber])
+        rightDie.image = UIImage(named: dieArray[rightNumber])
+        
+        total.text = "\(sum)"
+        
+        
+    }
 
 }
 
